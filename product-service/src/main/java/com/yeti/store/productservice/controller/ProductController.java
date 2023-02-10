f797a73724713ed54b3327ed736bb038f5295b5b
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-02-10 18:21:31 +0000

    Refactoring

--- product-service/src/main/java/com/yeti/store/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/yeti/store/productservice/controller/ProductController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
