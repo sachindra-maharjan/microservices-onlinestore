d658546fce49fd8ce8456c34761f552d2038b436
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-02-10 18:19:53 +0000

    Inventory service completed.

--- inventory-service/src/main/java/com/yeti/store/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/yeti/store/inventoryservice/controller/InventoryController.java
@@ -9,15 +9,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yeti.store.inventoryservice.dto.InventoryDto;
+import com.yeti.store.inventoryservice.exception.ServiceException;
+import com.yeti.store.inventoryservice.service.InventoryService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
     
+    private final InventoryService inventoryService;
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<InventoryDto> isAvailable(List<String> skus) {
-        return null;
+    public List<InventoryDto> isAvailable(List<String> skus) throws ServiceException{
+        return inventoryService.isInStock(skus);
     }
 
 }
--- inventory-service/src/main/java/com/yeti/store/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/yeti/store/inventoryservice/model/Inventory.java
@@ -5,13 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Table(name = "t_inventory")
 @Data
-@Builder
 public class Inventory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
